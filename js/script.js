3ba305b05dce0c77bb6aaac324b5d207de1dafff
LiamGuedez <liamhayes9393@gmail.com>
2018-08-26 09:49:48 -0400

    Added pages to the website.
    I added a function that creates a new element and adds it to the page. That element is a set of buttons that work as pages to see the students.

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ for (let x = 0; x < h3s.length;  x++)
 // Add variables that store DOM elements you will need to reference and/or manipulate
 const studentsArray = document.querySelectorAll('.student-item');
 
+
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 // hides student before studentMin and after studentMax
@@ -32,13 +33,48 @@ const hideStudentsHelper = (param1, param2, action) =>
     }
 }
 
- // hideStudents(10,15,100);
- // hideStudents(10,21,100);
+// Creates and appends pages as pagination links
+function appendPages()
+{
+    const pagesNeeded = Math.ceil(studentsArray.length/10);
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
+    if (pagesNeeded < 1)
+    {
+      alert('No list of students was passed to paginationLinks()');
+      return;
+    }
 
+    let studentPages =  `
+    <div class="pagination">
+      <ul>
+        <li>
+          <a class="active" href="#">1</a>
+        </li>
+        `;
 
+      for (let x = 2 ; x <= pagesNeeded ; x++)
+      {
+        studentPages +=  `
+        <li>
+          <a href="#">${x}</a>
+        </li>
+        `;
+      }
+     studentPages +=  `
+      </ul>
+    </div>
+    `;
 
+    let newDivChild = document.createElement('div');
+    newDivChild.innerHTML = studentPages;
+    const pageDiv = document.querySelector('.page');
+    pageDiv.appendChild(newDivChild);
+}
+
+// temporary function calls
+
+hideStudents(10,15,100);
+appendPages();
 
 // Add functionality to the pagination buttons so that they show and hide the correct items
 // Tip: If you created a function above to show/hide list items, it could be helpful here
